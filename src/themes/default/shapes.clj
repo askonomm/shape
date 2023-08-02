1da63a0ceb384e8ba7aff1b01bebb7c57c987e26
Asko Nõmm <asko@repl.ee>
2023-08-02 23:14:03 +0300

    Add basic page Content Shape

--- src/themes/default/shapes.clj
+++ src/themes/default/shapes.clj
@@ -18,5 +18,18 @@
                                  :name "URL Slug"
                                  :prefix (str utils/*url* "/")})]}))
 
+(def page
+  (delay {:identifier :page
+          :name "Pages"
+          :singular-name "Page"
+          :identity-field :title
+          :fields [(fields/text {:identifier :title
+                                 :placeholder "Untitled ..."
+                                 :name "Page Title"})
+                   (fields/text {:identifier :url-slug
+                                 :name "URL Slug"
+                                 :prefix (str utils/*url* "/")})]}))
+
 (def shapes
-  [@blog-post])
+  [@blog-post
+   @page])
