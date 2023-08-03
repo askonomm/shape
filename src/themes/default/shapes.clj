435adc3f11a1360cfac3d861b505dc79a0703f53
Asko Nõmm <asko@repl.ee>
2023-08-03 21:50:26 +0300

    Fix shapes

--- src/themes/default/shapes.clj
+++ src/themes/default/shapes.clj
@@ -3,33 +3,30 @@
    [shape.fields :as fields]
    [shape.utils :as utils]))
 
-; the only reason `delay` is used here is that
-; `utils/*url*` will not be defined until runtime.
-; You don't otherwise need `delay` here.
-(def blog-post
-  (delay {:identifier :post
-          :name "Blog Posts"
-          :singular-name "Blog Post"
-          :identity-field :title
-          :fields [(fields/text {:identifier :title
-                                 :placeholder "Untitled ..."
-                                 :name "Post Title"})
-                   (fields/text {:identifier :url-slug
-                                 :name "URL Slug"
-                                 :prefix (str utils/*url* "/")})]}))
+(defn- blog-post []
+  {:identifier :post
+   :name "Blog Posts"
+   :singular-name "Blog Post"
+   :identity-field :title
+   :fields [(fields/text {:identifier :title
+                          :placeholder "Untitled ..."
+                          :name "Post Title"})
+            (fields/text {:identifier :url-slug
+                          :name "URL Slug"
+                          :prefix (str utils/*url* "/blog/")})]})
 
-(def page
-  (delay {:identifier :page
-          :name "Pages"
-          :singular-name "Page"
-          :identity-field :title
-          :fields [(fields/text {:identifier :title
-                                 :placeholder "Untitled ..."
-                                 :name "Page Title"})
-                   (fields/text {:identifier :url-slug
-                                 :name "URL Slug"
-                                 :prefix (str utils/*url* "/")})]}))
+(defn- page []
+  {:identifier :page
+   :name "Pages"
+   :singular-name "Page"
+   :identity-field :title
+   :fields [(fields/text {:identifier :title
+                          :placeholder "Untitled ..."
+                          :name "Page Title"})
+            (fields/text {:identifier :url-slug
+                          :name "URL Slug"
+                          :prefix (str utils/*url* "/")})]})
 
 (def shapes
-  [@blog-post
-   @page])
+  [(blog-post)
+   (page)])
