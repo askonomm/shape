793a840918b6bb39c45659e37c64347a7e297101
Asko Nõmm <asko@repl.ee>
2023-08-08 20:39:57 +0300

    Implement date field

--- src/shape/fields/date.clj
+++ src/shape/fields/date.clj
@@ -1,9 +1,26 @@
 (ns shape.fields.date)
 
+(defn- editable
+  "Render a date field as an editable view."
+  [{:keys [identifier content-id value] :as opts}]
+  [:label (:name opts)
+   [:div.date-field.editable-field
+    [:input {:type "date"
+             :value value
+             :name (name identifier)
+             :hx-post (str "/admin/api/content-item/" content-id "/update-field")
+             :hx-trigger "input changed delay:250ms"}]]])
+
+(defn- viewable
+  "Render a date field as a viewable view."
+  [{:keys [value]}]
+  [:div.date-field.viewable-field
+   [:span.value value]])
+
 (defn date
   "Instance of a date field."
   [{:keys [identifier placeholder] :as opts}]
   {:identifier identifier
    :placeholder placeholder
-   :editable nil #_(editable (merge opts %))
-   :viewable nil #_(viewable (merge opts %))})
+   :editable #(editable (merge opts %))
+   :viewable #(viewable (merge opts %))})
