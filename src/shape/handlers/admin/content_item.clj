7403a0a95f513a2bcbe3fac492550154f5446aae
Asko Nõmm <asko@repl.ee>
2023-08-10 22:55:04 +0300

    Improve code quality

--- src/shape/handlers/admin/content_item.clj
+++ src/shape/handlers/admin/content_item.clj
@@ -5,6 +5,12 @@
     [shape.data :as data]
     [shape.shapes :as shapes]))
 
+(defn- editable-field [editable identifier content-id]
+  [:div.field
+   (editable
+     {:value (:value (data/content-item-field content-id (name identifier)))
+      :content-id content-id})])
+
 (defn- content [request]
   (let [shape-identifier (-> request :path-params :identifier)
         shape-identifier-kw (keyword shape-identifier)
@@ -24,26 +30,21 @@
          (if (vector? field)
            [:div.horizontal-fields
             (for [{:keys [editable identifier]} field]
-              [:div.field
-               (editable
-                 {:value (:value (data/content-item-field content-id (name identifier)))
-                  :content-id content-id})])]
+              (editable-field editable identifier content-id))]
            (let [{:keys [editable identifier]} field]
-             [:div.field
-              (editable
-                {:value (:value (data/content-item-field content-id (name identifier)))
-                 :content-id content-id})])))]]]))
+             (editable-field editable identifier content-id))))]]]))
 
 (defn handler [request]
   (let [shape-identifier (-> request :path-params :identifier)
         shape-identifier-kw (keyword shape-identifier)
-        content-id (-> request :path-params :id)]
+        content-id (-> request :path-params :id)
+        js (shapes/js-injections-by-identifier request shape-identifier-kw)
+        css (shapes/css-injections-by-identifier request shape-identifier-kw)]
     (if (data/content-item-exists? content-id)
       (->admin-page
        [:div.container
         (sidebar request)
         (content request)]
-       {:css (concat ["shape/css/admin"]
-                     (shapes/css-injections-by-identifier request shape-identifier-kw))
-        :js (shapes/js-injections-by-identifier request shape-identifier-kw)})
+       {:css (concat ["shape/css/admin"] css)
+        :js js})
       (->redirect (str "/admin/content/" shape-identifier)))))
